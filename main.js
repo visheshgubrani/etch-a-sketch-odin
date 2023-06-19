75a405f06289aa17c239c0b0454bba27626b151d
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-06-19 22:31:08 +0530

    Created the base model

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 const container = document.getElementById('container');
+let isDrawing = false;
+
+function startDrawing() {
+    isDrawing = true;
+}
+
+function draw(event) {
+    if (!isDrawing) return;
+
+    const div = event.target;
+    div.style.backgroundColor = 'black';
+}
+
+function stopDrawing() {
+    isDrawing = false;
+}
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const div = document.createElement('div');
-        div.style.backgroundColor = 'red';
+        
         div.classList.add('div');
         container.appendChild(div);
+        div.addEventListener('mousedown', startDrawing);
+        div.addEventListener('mousemove', draw);
+        div.addEventListener('mouseup', stopDrawing);
+        
+
     }
     
 }
+
+
+
+
 
