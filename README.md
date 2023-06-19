b3791dcd1b764930c614cfa120f9637ba66b607a
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-06-19 21:51:56 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch-odin
+
+Started The Project. facing Difficulties in Creating layout as of now.
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container');
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.style.backgroundColor = 'red';
-    div.classList.add('div');
-    container.appendChild(div);
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'red';
+        div.classList.add('div');
+        container.appendChild(div);
+    }
+    
 }
 
